ecc3e563231b0ebfe582aee9970728b34585745e
Aman Soni <sonisir1920@gmail.com>
2026-05-24 13:36:45 +0000

    First Ui App

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (BaseController) => {
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"   // ✅ correct path
+], (BaseController, MessageToast) => {
   "use strict";
 
   return BaseController.extend("sap.practice.practice.controller.App", {
       onInit() {
+      },
+      onPress() {
+          MessageToast.show("Button pressed!");
       }
   });
 });
