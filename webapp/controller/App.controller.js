4bc5971cf726e4eddfe72971d011b60c14d3b062
Aman Soni <sonisir1920@gmail.com>
2026-05-25 16:19:37 +0000

    login form layout

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,26 +1,10 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
-    "use strict";
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
 
-    return Controller.extend("sap.practice.practice.controller.App", {
-getApp: function () {
-    return this.byId("app");
-},
-
-onBack: function () {
-    this.getApp().back();
-},
-
-goToFlex: function () { this.getApp().to(this.byId("page2")); },
-goToGrid: function () { this.getApp().to(this.byId("page3")); },
-goToPanel: function () { this.getApp().to(this.byId("page4")); },
-goToToolbar: function () { this.getApp().to(this.byId("page5")); },
-goToVBox: function () { this.getApp().to(this.byId("page6")); },
-goToHBox: function () { this.getApp().to(this.byId("page7")); },
-goToScroll: function () { this.getApp().to(this.byId("page8")); },
-goToSplitter: function () { this.getApp().to(this.byId("page9")); },
-goToForm: function () { this.getApp().to(this.byId("page10")); }
-
-    });
+  return BaseController.extend("sap.practice.practice.controller.App", {
+      onInit() {
+      }
+  });
 });
