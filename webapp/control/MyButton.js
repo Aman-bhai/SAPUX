fb17cab8a6197d2b130b109b50ab52205953bf94
Aman Soni <sonisir1920@gmail.com>
2026-05-27 16:51:35 +0000

    custom control

---
+++ webapp/control/MyButton.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/m/Button"
+], function (Button) {
+    "use strict";
+
+    return Button.extend("sap.practice.practice.control.MyButton", {
+        metadata: {},
+
+        renderer: {}
+    });
+});
